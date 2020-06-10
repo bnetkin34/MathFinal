b0a77f6bea2dd3784443f4bb7de0642db3d1f9c8
Aaronk2711 <aaronk271@gmail.com>
2020-06-09 22:47:54 -0400

    Aaron signing off for the night

--- Rules.js
+++ Rules.js
@@ -10,20 +10,23 @@ function SendTo(location) {
    window.location.href = url;
 }
 
-var quotientstep;
+var quotientStep;
+var productStep;
+var powerStep;
+var chainStep;
 
-function QuotientStep() {
+function quotientStep() {
   var quotienttable = document.getElementById("quotienttable");
-  if (typeof quotientstep !== 'undefined') {
-    quotientstep +=1;
-    if (quotientstep == 2) {
+  if (typeof quotientStep !== 'undefined') {
+    quotientStep +=1;
+    if (quotientStep == 2) {
       var row = quotienttable.insertRow(1);
       var cell1 = row.insertCell(0);
       var cell2 = row.insertCell(1);
       cell1.innerHTML = "Step 2:";
       cell2.innerHTML = "something";
     }
-    if (quotientstep == 3) {
+    if (quotientStep == 3) {
       var row = quotienttable.insertRow(2);
       var cell1 = row.insertCell(0);
       var cell2 = row.insertCell(1);
@@ -32,8 +35,95 @@ function QuotientStep() {
     }
   }
   else {
-    quotientstep = 1;
+    quotientStep = 1;
     var row = quotienttable.insertRow(0);
+    var cell1= row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "Step 1:";
+    cell2.innerHTML = "f(x) = (7x-4), g(x) = (4x<sup>2</sup>-2)"
+  }
+}
+
+function productStep() {
+  var producttable = document.getElementById("producttable");
+  if (typeof productStep !== 'undefined') {
+    productStep +=1;
+    if (productStep == 2) {
+      var row = producttable.insertRow(1);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      cell1.innerHTML = "Step 2:";
+      cell2.innerHTML = "something";
+    }
+    if (productStep == 3) {
+      var row = producttable.insertRow(2);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      cell1.innerHTML = "Step 3:";
+      cell2.innerHTML = "something else";
+    }
+  }
+  else {
+    productStep = 1;
+    var row = producttable.insertRow(0);
+    var cell1= row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "Step 1:";
+    cell2.innerHTML = "f(x) = (7x-4), g(x) = (4x<sup>2</sup>-2)"
+  }
+}
+
+function chainStep() {
+  var chaintable = document.getElementById("chaintable");
+  if (typeof chainStep !== 'undefined') {
+    chainStep +=1;
+    if (chainStep == 2) {
+      var row = chaintable.insertRow(1);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      cell1.innerHTML = "Step 2:";
+      cell2.innerHTML = "something";
+    }
+    if (chainStep == 3) {
+      var row = chaintable.insertRow(2);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      cell1.innerHTML = "Step 3:";
+      cell2.innerHTML = "something else";
+    }
+  }
+  else {
+    chainStep = 1;
+    var row = chaintable.insertRow(0);
+    var cell1= row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "Step 1:";
+    cell2.innerHTML = "f(x) = (7x-4), g(x) = (4x<sup>2</sup>-2)"
+  }
+}
+
+function powerStep() {
+  var powertable = document.getElementById("powertable");
+  if (typeof powerStep !== 'undefined') {
+    powerStep +=1;
+    if (powerStep == 2) {
+      var row = powertable.insertRow(1);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      cell1.innerHTML = "Step 2:";
+      cell2.innerHTML = "something";
+    }
+    if (powerStep == 3) {
+      var row = powertable.insertRow(2);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      cell1.innerHTML = "Step 3:";
+      cell2.innerHTML = "something else";
+    }
+  }
+  else {
+    powerStep = 1;
+    var row = powertable.insertRow(0);
     var cell1= row.insertCell(0);
     var cell2 = row.insertCell(1);
     cell1.innerHTML = "Step 1:";
