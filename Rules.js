5c0a802f7e8297d3c96d4ea7448c5e7890a9ad0b
bnetkin34 <sweatynettie34@gmail.com>
2020-06-10 14:08:16 -0400

    final commit

--- Rules.js
+++ Rules.js
@@ -2,8 +2,8 @@ function SendTo(location) {
    var url = window.location.href.toString();
    var splitaroo = url.split("/");
    url = "";
-   for (x = 0; x < splitaroo.length-1; x++ ) {
-    url += splitaroo[x] + "/"
+   for (t = 0; t < splitaroo.length-1; t++ ) {
+    url += splitaroo[t] + "/"
    }
    url += location;
    console.log(url);
@@ -14,6 +14,7 @@ var quotientStep;
 var productStep;
 var powerStep;
 var chainStep;
+var limStep;
 
 function powerStepFunction() {
   var powertable = document.getElementById("powertable");
@@ -24,14 +25,14 @@ function powerStepFunction() {
       var cell1 = row.insertCell(0);
       var cell2 = row.insertCell(1);
       cell1.innerHTML = "Step 2:";
-      cell2.innerHTML = "something";
+      cell2.innerHTML = "5 - 1 = 4";
     }
     if (powerStep == 3) {
       var row = powertable.insertRow(2);
       var cell1 = row.insertCell(0);
       var cell2 = row.insertCell(1);
       cell1.innerHTML = "Step 3:";
-      cell2.innerHTML = "something else";
+      cell2.innerHTML = "Final Answer: 60t<sup>4</sup>";
     }
   }
   else {
@@ -40,7 +41,7 @@ function powerStepFunction() {
     var cell1= row.insertCell(0);
     var cell2 = row.insertCell(1);
     cell1.innerHTML = "Step 1:";
-    cell2.innerHTML = "f(x) = (7x-4), g(x) = (4x<sup>2</sup>-2)"
+    cell2.innerHTML = "12 * 5 = 60"
   }
 }
 
@@ -53,14 +54,14 @@ function quotientStepFunction() {
       var cell1 = row.insertCell(0);
       var cell2 = row.insertCell(1);
       cell1.innerHTML = "Step 2:";
-      cell2.innerHTML = "something";
+      cell2.innerHTML = "h'(t) = ((4t<sup>2</sup> - 2)(d/dt(7t-4)) - (7t-4)(d/dt(4t<sup>2</sup> - 2)))/(4t<sup>2</sup> - 2)<sup>2</sup>";
     }
     if (quotientStep == 3) {
       var row = quotienttable.insertRow(2);
       var cell1 = row.insertCell(0);
       var cell2 = row.insertCell(1);
       cell1.innerHTML = "Step 3:";
-      cell2.innerHTML = "something else";
+      cell2.innerHTML = "h'(t) = (28t<sup>2</sup> - 14 - 56t<sup>2</sup> + 32t)/(4t<sup>2</sup> - 2)<sup>2</sup></br> h'(t) = -28t<sup>2</sup> + 32t - 14/(4t<sup>2</sup> - 2)<sup>2</sup> ";
     }
   }
   else {
@@ -69,7 +70,7 @@ function quotientStepFunction() {
     var cell1= row.insertCell(0);
     var cell2 = row.insertCell(1);
     cell1.innerHTML = "Step 1:";
-    cell2.innerHTML = "f(x) = (7x-4), g(x) = (4x<sup>2</sup>-2)"
+    cell2.innerHTML = "f(t) = (7t-4), g(t) = (4t<sup>2</sup>-2), f '(t) = 7, g'(t) = 8t"
   }
 }
 
@@ -82,14 +83,14 @@ function productStepFunction() {
       var cell1 = row.insertCell(0);
       var cell2 = row.insertCell(1);
       cell1.innerHTML = "Step 2:";
-      cell2.innerHTML = "something";
+      cell2.innerHTML = "h'(t) = f(t)g'(t) + f '(t)g(t) </br> h'(t) = (4x<sup>2</sup>)(d/dt9x<sup>3</sup>) + (d/dt4x<sup>2</sup>)(9x<sup>3</sup> - 1)";
     }
     if (productStep == 3) {
       var row = producttable.insertRow(2);
       var cell1 = row.insertCell(0);
       var cell2 = row.insertCell(1);
       cell1.innerHTML = "Step 3:";
-      cell2.innerHTML = "something else";
+      cell2.innerHTML = "h'(t) = (4x<sup>2</sup>)(27x<sup>2</sup>) + (8x)(9x<sup>3</sup> - 1)</br> h'(t) = 108x<sup>4</sup> + 72<sup>4</sup> - 8x = 180x<sup>4</sup> - 8x";
     }
   }
   else {
@@ -98,7 +99,7 @@ function productStepFunction() {
     var cell1= row.insertCell(0);
     var cell2 = row.insertCell(1);
     cell1.innerHTML = "Step 1:";
-    cell2.innerHTML = "f(x) = (7x-4), g(x) = (4x<sup>2</sup>-2)"
+    cell2.innerHTML = "f(t) = 4t<sup>2</sup>, g(t) = 9t<sup>3</sup>, f '(t) = 8t, g'(t) = 27t<sup>2</sup>"
   }
 }
 
@@ -111,14 +112,14 @@ function chainStepFunction() {
       var cell1 = row.insertCell(0);
       var cell2 = row.insertCell(1);
       cell1.innerHTML = "Step 2:";
-      cell2.innerHTML = "d/dt = 3(5t + 2)<sup>2</sup> * 5</br>d/dt(5t + 2) = 5";
+      cell2.innerHTML = "u' = d/dt(5t + 2) = 5</br> dy/dt = 15(5t + 2)<sup>2</sup>";
     }
     if (chainStep == 3) {
       var row = chaintable.insertRow(2);
       var cell1 = row.insertCell(0);
       var cell2 = row.insertCell(1);
       cell1.innerHTML = "Step 3:";
-      cell2.innerHTML = "Final Answer: d/dt = (5t + 2)<sup>3</sup></br> 15(5t + 2)<sup>2</sup>";
+      cell2.innerHTML = "Final Answer: dy/dt = 15(5(7) + 2)<sup>2</sup> = 20,535 ms<sup>-2</sup>";
     }
   }
   else {
@@ -127,7 +128,7 @@ function chainStepFunction() {
     var cell1= row.insertCell(0);
     var cell2 = row.insertCell(1);
     cell1.innerHTML = "Step 1:";
-    cell2.innerHTML = "d/dt(5x +2)<sup>3</sup> = 3 * 5(5x + 2)<sup>2</sup>"
+    cell2.innerHTML = "d/dt(u)<sup>3</sup> = 3u<sup>2</sup>u'"
   }
 }
 
@@ -140,14 +141,14 @@ function aodstepFunction() {
       var cell1 = row.insertCell(0);
       var cell2 = row.insertCell(1);
       cell1.innerHTML = "Step 2:";
-      cell2.innerHTML = "Intervals: (-2, -(1/2)), ((-1/2), 2)</br> Test Points: -1, 1 </br> At x = -1, the derivative is negative which means the function is decreasing </br> At x = 1, the derivative is positive which means the function is increasing";
+      cell2.innerHTML = "Intervals: [-2, -1/2], [-1/2, 2]</br> Test Points: -1, 1 </br> At t = -1, the derivative is negative which means the function is decreasing </br> At t = 1, the derivative is positive which means the function is increasing";
     }
     if (aodStep == 3) {
       var row = aodtable.insertRow(2);
       var cell1 = row.insertCell(0);
       var cell2 = row.insertCell(1);
       cell1.innerHTML = "Step 3:";
-      cell2.innerHTML = "On the left side of our critical point, the derivative is negative. On the right side of our critical point, the derivative is positive. Since the functions derivative is going from negative to positive at this point, it means the point x = (-1/2) is a relative minimum.";
+      cell2.innerHTML = "On the left side of our critical point, the derivative is negative. On the right side of our critical point, the derivative is positive. Because the function's derivative is going from negative to positive at this point, the point t = (-1/2) is a relative minimum.";
     }
   }
   else {
@@ -156,6 +157,28 @@ function aodstepFunction() {
     var cell1= row.insertCell(0);
     var cell2 = row.insertCell(1);
     cell1.innerHTML = "Step 1:";
-    cell2.innerHTML = "f'(x) = 2x+1(power rule)</br>2x + 1 = 0 </br> x = -(1/2) (critical point)"
+    cell2.innerHTML = "f '(t) = 2t+1(power rule)</br>2t + 1 = 0 </br> t = -(1/2) (critical point)"
+  }
+}
+
+function limStepFunction(){
+  var limtable = document.getElementById("limtable");
+  if (typeof limStep !== 'undefined') {
+    limStep +=1;
+    if (limStep == 2) {
+      var row = limtable.insertRow(1);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      cell1.innerHTML = "Step 2:";
+      cell2.innerHTML = "Final Answer: Limit as x approaches 9 of (&#8730;x - 3)/(x - 9)";
+    }
+  }
+  else {
+    limStep = 1;
+    var row = limtable.insertRow(0);
+    var cell1= row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "Step 1:";
+    cell2.innerHTML = "Limit as x approaches t of (f(x) - f(t))/(x - t) = Limit as x approaches 9 of (&#8730;x - &#8730;9)/(x - 9)"
   }
 }
